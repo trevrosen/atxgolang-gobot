d880d054e1126c7fba18beb2f804b3d9a128bb7a
Trevor Rosen <trevor@catapult-creative.com>
2015-07-20 22:18:32 -0500

    Add buzzer

--- example.go
+++ example.go
@@ -13,21 +13,36 @@ func main() {
 	gbot := gobot.NewGobot()
 
 	e := edison.NewEdisonAdaptor("edison")
-	led := gpio.NewLedDriver(e, "led", "13")
 
-	work := func() {
+	led := gpio.NewLedDriver(e, "led", "13")
+	buzzer := gpio.NewBuzzerDriver(e, "buzzer", "4")
+
+	blink_work := func() {
 		gobot.Every(10*time.Second, func() {
 			led.Toggle()
 		})
 	}
 
-	robot := gobot.NewRobot("blinkBot",
+	buzz_work := func() {
+		gobot.Every(5*time.Second, func() {
+			// make it buzz
+		})
+	}
+
+	blink_bot := gobot.NewRobot("blinkBot",
 		[]gobot.Connection{e},
 		[]gobot.Device{led},
-		work,
+		blink_work,
 	)
 
-	gbot.AddRobot(robot)
+	buzz_bot := gobot.NewRobot("buzzBot",
+		[]gobot.Connection{e},
+		[]gobot.Device{buzzer},
+		buzz_work,
+	)
+
+	gbot.AddRobot(blink_bot)
+	gbot.AddRobot(buzz_bot)
 
 	a := api.NewAPI(gbot)
 	a.Debug()
